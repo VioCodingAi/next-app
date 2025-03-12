c32d5b35ef0e15bb55b8f194b412cfc6b73e4607
VioCodingAi <vivoviva@gmail.com>
2025-03-13 07:37:13 +0800

    Refactor UsersPage to use UserTable component and add UserDetailPage and PhotoPage components

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  params: Promise<{ id: number }>;
+}
+
+const UserDetailPage = async ({ params }: Props) => {
+  const { id } = await params;
+
+  return <div>UserDetailPage {id}</div>;
+};
+
+export default UserDetailPage;
