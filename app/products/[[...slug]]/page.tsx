38e9ffa3a6e5380328fa4c2a784e1f23652f0a92
VioCodingAi <vivoviva@gmail.com>
2025-03-13 07:43:59 +0800

    Add ProductPage component to display product details based on slug

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  params: Promise<{ slug: string[] }>;
+}
+
+const ProductPage = async ({ params }: Props) => {
+  const { slug } = await params;
+
+  return <div>ProductPage {slug}</div>;
+};
+
+export default ProductPage;
