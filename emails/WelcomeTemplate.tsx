9eeefa821a4f7914d8ca12bf8d63843a73ff8fb2
VioCodingAi <vivoviva@gmail.com>
2025-03-15 08:07:50 +0800

    Add email sending functionality with welcome template; implement POST route and update package dependencies

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,27 @@
+import {
+  Html,
+  Body,
+  Container,
+  Tailwind,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome aboard!</Preview>
+      <Tailwind>
+        <Body>
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
